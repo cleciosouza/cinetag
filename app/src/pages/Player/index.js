66c86128a8a5ee03ecb707eb917dca54b035f283
cleciosouza <cleciodesign@gmail.com>
2024-07-16 17:19:44 -0300

    Subindo aplicação para o repositorio

---
+++ app/src/pages/Player/index.js
@@ -0,0 +1,47 @@
+import Banner from "components/Banner";
+import styles from "./Player.module.css"
+import Titulo from "components/Titulo";
+import { useParams } from "react-router-dom";
+import NaoEncontrado from "pages/NaoEncontrado";
+import { useEffect, useState } from "react";
+
+function Player() {
+    const [video, setVideo] = useState()
+    const parametros = useParams();
+    // const video = videos.find((video) => {
+    //     return video.id === Number(parametros.id);
+    // })
+    useEffect(() => {
+        fetch(`https://my-json-server.typicode.com/cleciosouza/cinetag-api/videos?id=${parametros.id}`)
+        .then(resposta => resposta.json())
+        .then(dados => {
+            setVideo(...dados)
+        })
+    }, [])
+    if (!video) {
+        return <NaoEncontrado />
+    }
+
+    console.log(video)
+    return (
+        <>
+            <Banner imagem="player"/>
+            <Titulo>
+                <h1>Player</h1>
+            </Titulo>
+            <section className={styles.container}>
+                <iframe 
+                    width="100%" 
+                    height="100%" 
+                    src={video.link}
+                    title={video.titulo} 
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerpolicy="strict-origin-when-cross-origin" 
+                    allowfullscreen></iframe>
+            </section>
+        </>
+    )
+}
+
+export default Player;
